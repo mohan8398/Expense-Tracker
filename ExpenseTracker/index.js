3e027bd8ddd424c5f3b1209c1e07338d35f74696
mohan8398 <98051444+mohan8398@users.noreply.github.com>
2022-04-24 00:21:00 +0530

    Update index.js

--- ExpenseTracker/index.js
+++ ExpenseTracker/index.js
@@ -21,3 +21,96 @@ function addNewExpense(e){
     }).catch(err => showError(err))
 
 }
+
+window.addEventListener('load', ()=> {
+    axios.get('http://localhost:3000/user/getexpenses', { headers: {"Authorization" : token} }).then(response => {
+        if(response.status === 200){
+            response.data.expenses.forEach(expense => {
+
+                addNewExpense(expense);
+            })
+        } else {
+            throw new Error();
+        }
+    })
+});
+
+function addNewExpensetoUI(expense){
+    const parentElement = document.getElementById('listOfExpenses');
+    const expenseElemId = `expense-${expense.id}`;
+    parentElement.innerHTML += `
+        <li id=${expenseElemId}>
+            ${expense.expenseamount} - ${expense.category} - ${expense.description}
+            <button onclick='deleteExpense(event, ${expense.id})'>
+                Delete Expense
+            </button>
+        </li>`
+}
+
+function deleteExpense(e, expenseid) {
+    axios.delete(`http://localhost:3000/user/deleteexpense/${expenseid}`, { headers: {"Authorization" : token} }).then((response) => {
+
+    if(response.status === 204){
+            removeExpensefromUI(expenseid);
+        } else {
+            throw new Error('Failed to delete');
+        }
+    }).catch((err => {
+        showError(err);
+    }))
+}
+
+function showError(err){
+    document.body.innerHTML += `<div style="color:red;"> ${err}</div>`
+}
+
+function removeExpensefromUI(expenseid){
+    const expenseElemId = `expense-${expenseid}`;
+    document.getElementById(expenseElemId).remove();
+}
+
+
+
+document.getElementById('rzp-button1').onclick = async function (e) {
+    const response  = await axios.get('http://localhost:3000/purchase/premiummembership', { headers: {"Authorization" : token} });
+    console.log(response);
+    var options =
+    {
+     "key": response.data.key_id, // Enter the Key ID generated from the Dashboard
+     "name": "Test Company",
+     "order_id": response.data.order.id, // For one time payment
+     "prefill": {
+       "name": "Test User",
+       "email": "test.user@example.com",
+       "contact": "7003442036"
+     },
+     "theme": {
+      "color": "#3399cc"
+     },
+     // This handler function will handle the success payment
+     "handler": function (response) {
+         console.log(response);
+         axios.post('http://localhost:3000/purchase/updatetransactionstatus',{
+             order_id: options.order_id,
+             payment_id: response.razorpay_payment_id,
+         }, { headers: {"Authorization" : token} }).then(() => {
+             alert('You are a Premium User Now')
+         }).catch(() => {
+             alert('Something went wrong. Try Again!!!')
+         })
+     },
+  };
+  const rzp1 = new Razorpay(options);
+  rzp1.open();
+  e.preventDefault();
+
+  rzp1.on('payment.failed', function (response){
+  alert(response.error.code);
+  alert(response.error.description);
+  alert(response.error.source);
+  alert(response.error.step);
+  alert(response.error.reason);
+  alert(response.error.metadata.order_id);
+  alert(response.error.metadata.payment_id);
+ });
+}
