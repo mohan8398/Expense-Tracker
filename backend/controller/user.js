1cf481cf82f700ff2f6ececc876289fc903b6463
mohan8398 <98051444+mohan8398@users.noreply.github.com>
2022-04-23 15:01:45 +0530

    Update user.js

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -1,5 +1,7 @@
 const bcrypt = require('bcrypt');
 
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/users');
 
  const signup = (req, res)=>{
@@ -21,7 +23,38 @@ const User = require('../models/users');
         });
     });
 }
+function generateAccessToken(id) {
+    return jwt.sign(id ,process.env.TOKEN_SECRET);
+}
 
+const login = (req, res) => {
+    const { email, password } = req.body;
+    console.log(password);
+    User.findAll({ where : { email }}).then(user => {
+        if(user.length > 0){
+            bcrypt.compare(password, user[0].password, function(err, response) {
+                if (err){
+                console.log(err)
+                return res.json({success: false, message: 'Something went wrong'})
+                }
+                if (response){
+                    console.log(JSON.stringify(user))
+                    const jwttoken = generateAccessToken(user[0].id);
+                    res.json({token: jwttoken, success: true, message: 'Successfully Logged In'})
+                // Send JWT
+                } else {
+                // response is OutgoingMessage object that server response http request
+                return res.status(401).json({success: false, message: 'passwords do not match'});
+                }
+            });
+        } else {
+            return res.status(404).json({success: false, message: 'passwords do not match'})
+        }
+    })
+}
 
+module.exports = {
+    signup,
+    login,
 
-module.exports = signup;
+}
