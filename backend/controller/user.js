6fafdf337853f1f04b6dd0044c7e8cd44c93685b
mohan8398 <98051444+mohan8398@users.noreply.github.com>
2022-04-23 15:00:15 +0530

    Create user.js

---
+++ backend/controller/user.js
@@ -0,0 +1,27 @@
+const bcrypt = require('bcrypt');
+
+const User = require('../models/users');
+
+ const signup = (req, res)=>{
+    const { name, email, password } = req.body;
+    const saltRounds = 10;
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+        bcrypt.hash(password, salt, function(err, hash) {
+            // Store hash in your password DB.
+            if(err){
+                console.log('Unable to create new user')
+                res.json({message: 'Unable to create new user'})
+            }
+            User.create({ name, email, password: hash }).then(() => {
+                res.status(201).json({message: 'Successfuly create new user'})
+            }).catch(err => {
+                res.status(403).json(err);
+            })
+
+        });
+    });
+}
+
+
+
+module.exports = signup;
