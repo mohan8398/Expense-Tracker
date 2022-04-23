7ab5fe7f542041c6443aa574199c082cb400aceb
mohan8398 <98051444+mohan8398@users.noreply.github.com>
2022-04-23 15:11:43 +0530

    Create app.js

---
+++ backend/app.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+const express = require('express');
+var cors = require('cors')
+const sequelize = require('./util/database');
+const User = require('./models/users');
+const Expense = require('./models/expenses');
+
+const userRoutes = require('./routes/user')
+
+const app = express();
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+app.use(cors());
+
+
+app.use(express.json()); 
+
+app.use('/user', userRoutes)
+
+User.hasMany(Expense);
+Expense.belongsTo(User);
+
+
+sequelize.sync()
+    .then(() => {
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err);
+    })
