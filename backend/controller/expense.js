64cb8e10aef0ed8cb03ea6c6cc08f08bae195201
mohan8398 <98051444+mohan8398@users.noreply.github.com>
2022-04-24 03:33:06 +0530

    Update expense.js

--- backend/controller/expense.js
+++ backend/controller/expense.js
@@ -18,8 +18,61 @@ const getexpenses = (req, res)=> {
         return res.status(402).json({ error: err, success: false})
     })
 }
+exports.downloadExpense = async (req, res, next) => {
 
+    console.log("Inside download Expenses");
+
+    try {
+
+        const expenses = await req.user.getExpenses();
+        console.log('expenses in downloadExpenses>>> ', expenses);
+        const stringifiedExpenses = JSON.stringify(expenses);
+
+        const fileName = `Expense-${req.user.id}-${new Date()}.txt`;
+
+        const fileUrl = await uploadToS3(stringifiedExpenses, fileName);
+
+        console.log("file URL >>> ", fileUrl);
+
+        await req.user.createExpensefile({
+            url: fileUrl,
+            name: fileName,
+        });
+
+        res.status(200).json({ success: true, url: fileUrl });
+    } catch (err) {
+        res.status(500).json({ success: false, error: err });
+    }
+}
+
+
+const uploadToS3 = (data, name) => {
+    console.log('inside upload to S3 >>>> ')
+
+    const s3 = new AWS.S3({
+        accessKeyId: process.env.IAM_USERID,
+        secretAccessKey: process.env.IAM_SECRET_KEY,
+    });
+
+    const params = {
+        Bucket: process.env.S3_BUCKET,
+        Key: name,
+        Body: data,
+        ACL: "public-read",
+    };
+
+    return new Promise((resolve, reject) => {
+        s3.upload(params, (err, data) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(data.Location);
+        });
+    });
+
+}
 module.exports = {
     getexpenses,
-    addexpense
+    addexpense,
+   
 }
