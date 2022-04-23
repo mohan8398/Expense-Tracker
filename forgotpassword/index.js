1fae51c5e3e34ae79042442201999872b192e287
mohan8398 <98051444+mohan8398@users.noreply.github.com>
2022-04-24 02:12:26 +0530

    Update index.js

--- forgotpassword/index.js
+++ forgotpassword/index.js
@@ -8,5 +8,14 @@ function forgotpassword(e) {
 
     }
     console.log(userDetails)
-    axios.post('http://localhost:3000/password/forgotpassword',userDetails);
+    axios.post('http://localhost:3000/password/forgotpassword',userDetails).then(response => {
+        if(response.status === 202){
+            document.body.innerHTML += '<div style="color:red;">Mail Successfuly sent <div>'
+        } else {
+            throw new Error('Something went wrong!!!')
+        }
+    }).catch(err => {
+        document.body.innerHTML += `<div style="color:red;">${err} <div>`;
+    })
 }
+
