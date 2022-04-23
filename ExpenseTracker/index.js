b888f719cab00519e863c061de3f4f68419ddff1
mohan8398 <98051444+mohan8398@users.noreply.github.com>
2022-04-23 15:43:54 +0530

    Create index.js

---
+++ ExpenseTracker/index.js
@@ -0,0 +1,23 @@
+function addNewExpense(e){
+    e.preventDefault();
+    const form = new FormData(e.target);
+
+    const expenseDetails = {
+        expenseamount: form.get("expenseamount"),
+        description: form.get("description"),
+        category: form.get("category")
+
+    }
+    const token = localStorage.getItem('token');
+    console.log(expenseDetails)
+    axios.post('http://localhost:3000/user/addexpense',expenseDetails, { headers: {"Authorization" : token} }).then((response) => {
+
+    if(response.status === 201){
+        addNewExpensetoUI(response.data.expense);
+    } else {
+        throw new Error('Failed To create new expense');
+    }
+
+    }).catch(err => showError(err))
+
+}
